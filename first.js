3eda0af7193a77a85c3b5027ba0d4cf34c4e40e7
fehmida9 <fhmdblch@gmail.com>
2024-11-11 21:13:09 +0500

    js
    javascript

---
+++ first.js
@@ -0,0 +1,41 @@
+// console.log("well come to apna colleg");
+// console.log("Fehmida");
+// console.log("hello js"); 
+
+// fullname ="Fehmida";
+// age = 19;
+// price = 99.9;
+// radius = 14;
+// a = null; 
+// y = undefined ;
+// isfollow = true;
+//  console.log(y);
+
+//  fullname = "fehmida";
+// fullname = "maheen";
+// console.log (fullname);
+
+// let fullName = "Fehmida";
+// let age = 19 ;
+// let totalPrice = 1000;
+// console.log(fullName);
+
+//const age = 19;
+//const PI =3.4;
+//console.log(age);
+
+//{
+    //let a = 5;
+  //  console.log(a);
+//}
+//{
+//let a = 10;
+//console.log(a);
+//}
+const student = {
+    fullName: "Fehmida",
+    age: 19 ,
+    cgpa: 8.2 ,
+    ispass: true,
+};
+console.log(student ["age"]);
